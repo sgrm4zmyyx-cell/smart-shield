9f7acbd13b13c386e5792b4c1f99492057cb53f5
sgrm4zmyyx-cell <sxrm4zmyyx@privaterelay.appleid.com>
2026-05-13 10:26:48 +0300

    Create app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0D47A1);
+  static const Color primaryLight = Color(0xFF1565C0);
+  static const Color primaryDark = Color(0xFF0A2F6B);
+  static const Color danger = Color(0xFFB71C1C);
+  static const Color dangerLight = Color(0xFFE53935);
+  static const Color warning = Color(0xFFF57F17);
+  static const Color success = Color(0xFF1B5E20);
+  static const Color successLight = Color(0xFF43A047);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color surface = Color(0xFFF5F7FA);
+  static const Color border = Color(0xFFE0E6ED);
+  static const Color textPrimary = Color(0xFF1A2332);
+  static const Color textSecondary = Color(0xFF6B7A8D);
+  static const Color locked = Color(0xFFBDBDBD);
+  static const Color lockedBg = Color(0xFFF5F5F5);
+}
